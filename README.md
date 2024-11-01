c5880273eb4ca7400c84bc24c2ca643d3227e7bf
yjgaia <yjdev88@gmail.com>
2024-11-01 21:52:45 +0900

    Add initial setup for Supabase integration and metadata table creation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # godmode
 An exclusive membership unlocking premium benefits and divine privileges. Reserved for The Gods NFT collectors and powerful GAIA token holders.
+
+```
+cd utils
+node --loader ts-node/esm ./insert-init-data.ts
+```
---
+++ utils/insert-init-data.ts
@@ -0,0 +1,31 @@
+import { createClient } from "@supabase/supabase-js";
+import data from "./data.json" assert {
+  type: "json",
+};
+import "dotenv/config";
+
+const SUPABASE_URL = process.env.SUPABASE_URL!;
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY!;
+
+async function main() {
+  const client = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
+  for (const god of data) {
+    await client.from("the_gods_metadatas").upsert({
+      id: god._id,
+      type: god.type,
+      gender: god.gender,
+      parts: god.parts,
+    });
+
+    console.log(`Inserted ${god._id}`);
+  }
+
+  console.log("Data inserted");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
