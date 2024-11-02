f544694d02fd66409562a7e63f934528e4d511c1
yjgaia <yjdev88@gmail.com>
2024-11-02 12:23:26 +0900

    Rename primary key from 'id' to 'token_id' in the_gods_metadatas table

--- database/create-table/the_gods_metadatas.sql
+++ database/create-table/the_gods_metadatas.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS "public"."the_gods_metadatas" (
-  "id" bigint NOT NULL,
+  "token_id" bigint NOT NULL,
   "type" "text" NOT NULL,
   "gender" "text" NOT NULL,
   "parts" "jsonb" NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS "public"."the_gods_metadatas" (
 ALTER TABLE "public"."the_gods_metadatas" OWNER TO "postgres";
 
 ALTER TABLE ONLY "public"."the_gods_metadatas"
-  ADD CONSTRAINT "the_gods_metadatas_pkey" PRIMARY KEY ("id");
+  ADD CONSTRAINT "the_gods_metadatas_pkey" PRIMARY KEY ("token_id");
 
 ALTER TABLE "public"."the_gods_metadatas" ENABLE ROW LEVEL SECURITY;
 
