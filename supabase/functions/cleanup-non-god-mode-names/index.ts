e38c2de948a4df2c5255eff6863915d8e772cfeb
yjgaia <yjdev88@gmail.com>
2024-11-13 20:43:58 +0900

    Refactor cleanup-non-god-mode-names function to use getGodBalances; update data fetching and storage logic for improved clarity and efficiency

--- supabase/functions/cleanup-non-god-mode-names/index.ts
+++ supabase/functions/cleanup-non-god-mode-names/index.ts
@@ -1,18 +1,26 @@
+import { getGodBalances } from "https://raw.githubusercontent.com/gaiaprotocol/godmode/refs/heads/main/deno/godmode.ts";
 import { serve } from "https://raw.githubusercontent.com/yjgaia/deno-module/refs/heads/main/api.ts";
-import { safeFetch } from "https://raw.githubusercontent.com/yjgaia/supabase-module/refs/heads/main/deno/supabase.ts";
-import { getGodBalanceList } from "https://raw.githubusercontent.com/gaiaprotocol/godmode/refs/heads/main/deno/godmode.ts";
+import {
+  safeFetch,
+  safeStore,
+} from "https://raw.githubusercontent.com/yjgaia/supabase-module/refs/heads/main/deno/supabase.ts";
 
-serve(async (req) => {
-  console.log("req", req);
-
-  const names = await safeFetch<{ wallet_address: string; name: string }[]>(
+serve(async () => {
+  const dataSet = await safeFetch<{ wallet_address: string }[]>(
     "gaia_names",
-    (b) => b.select("*"),
+    (b) => b.select("wallet_address"),
   );
 
-  const walletAddresses = names.map((n) => n.wallet_address);
+  const walletAddresses = dataSet.map((n) => n.wallet_address);
+  const balances = await getGodBalances(walletAddresses);
+  const nonGodModeWalletAddresses = walletAddresses.filter(
+    (wa) => balances[wa] === 0n,
+  );
 
-  const balances = await getGodBalanceList(walletAddresses);
+  await safeStore(
+    "gaia_names",
+    (b) => b.delete().in("wallet_address", nonGodModeWalletAddresses),
+  );
 
   return "OK";
 });
