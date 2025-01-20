5aaf9da176cdd160950e223b69e070b487bcf9ba
yjgaia <yjdev88@gmail.com>
2025-01-20 18:14:04 +0900

    Simplify check-jwt-token function by removing unnecessary async and return statement

--- supabase/functions/check-jwt-token/index.ts
+++ supabase/functions/check-jwt-token/index.ts
@@ -1,6 +1,4 @@
 import { serve } from "https://raw.githubusercontent.com/yjgaia/deno-module/refs/heads/main/api.ts";
 import { extractWalletAddressFromRequest } from "https://raw.githubusercontent.com/yjgaia/wallet-login-module/refs/heads/main/deno/auth.ts";
 
-serve(async (req) => {
-  return extractWalletAddressFromRequest(req);
-});
+serve((req) => extractWalletAddressFromRequest(req));
